4f512e49291300bf922bb8594fe77b96a6b8d8a4
thextool <matthew.riach@gmail.com>
2019-10-17 14:47:33 -0600

    first commit by matt. Added file structure and files. Ready to start codinggit add .git add .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import BudgetItem from './budgetItem/BudgetItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>RnD Time Buget</h1>
-        <BudgetItem />
       </header>
     </div>
   );
